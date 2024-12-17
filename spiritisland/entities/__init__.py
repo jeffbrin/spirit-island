5bd1a58c137e1e86c65cddb9d98063db7b806c4d
JeffBrin <bringolfj@hotmail.com>
2024-12-17 18:19:20 -0500

    Added blight class

--- spiritisland/entities/__init__.py
+++ spiritisland/entities/__init__.py
@@ -2,6 +2,7 @@ from .entity import Entity
 from .dahan import Dahan
 from .invaders import Explorer, Town, City, Invader
 from .presence import Presence
+from .blight import Blight
 
 assert Entity
 assert Dahan
@@ -10,3 +11,4 @@ assert Town
 assert City
 assert Invader
 assert Presence
+assert Blight
---
+++ spiritisland/entities/blight.py
@@ -0,0 +1,6 @@
+from .entity import Entity
+
+class Blight(Entity):
+    def __init__(self):
+        super().__init__()
+        
