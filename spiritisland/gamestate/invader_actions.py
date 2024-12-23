f39122257d7d16013b1ebb75bbd81308d29855d6
JeffBrin <bringolfj@hotmail.com>
2024-12-23 15:54:51 -0500

    Touched up invader actions

--- spiritisland/gamestate/invader_actions.py
+++ spiritisland/gamestate/invader_actions.py
@@ -1,43 +1,40 @@
-from enums import LandType, Entities
+from enums import LandType
 from board import Land, Board
-from .gamestate import GameState
-from blight_pool import BlightPool
 from invader_cards import InvaderCard
+from entities import Explorer, Town, City
 
-
-class InvaderActions(GameState):
+class InvaderActions():
     def __init__(self, ravage_land: InvaderCard, build_land: InvaderCard, explore_land: InvaderCard):
         self.ravage_land = ravage_land
         self.build_land = build_land
         self.explore_land = explore_land
 
-    def ravage(self, ravage_land: list[LandType]):
-        for land in Board.lands:
+    def ravage(self, board: Board, ravage_land: list[LandType]):
+        for land in board.lands.values():
             if land.land_type in ravage_land:
                 land.take_damage_from_invaders()
     
-    def explore(self, explore_land: InvaderCard):
+    def explore(self, board: Board, explore_card: InvaderCard):
         def neighbour_city_town(land: Land):
-            for i in land.neighbours:
-                if len(i.towns) > 0 or len(i.cities) > 0:
+            for neighbor in land.neighbours:
+                if len(neighbor.towns) > 0 or len(neighbor.cities) > 0:
                     return True
                 else:
                     return False
                 
-        for land in Board.lands:
-            # TODO: Fix land in explore_land
-            # since explore_land is not a list of lands
-            if  land in explore_land and (
-                land.invader_damage != 0 or\
-                land.coastal == True or\
-                neighbour_city_town(land) == True):
-                land.add(Entities.EXPLORER)
+        for land in board.lands.values():
+            if  land.land_type in explore_card.land_types and (
+                    land.invader_damage > 0 or \
+                    land.coastal or \
+                    neighbour_city_town(land)):
+                land.add(Explorer())
 
-    def build(self, build_land):
-        for land in Board.lands:
-            if land in build_land and (len(land.towns) > 0) or (len(land.towns) > 0):
+    def build(self, board: Board, build_card: InvaderCard):
+        for land in board.lands.values():
+            if land.land_type in build_card.land_types and \
+                    land.invader_damage > 0:
                 if len(land.towns) >= len(land.cities):
-                    land.add(Entities.CITY)
+                    land.add(City())
                 else:
-                    land.add(Entities.TOWN)
+                    land.add(Town())
     
--- spiritisland/gamestate/invader_cards.py
+++ spiritisland/gamestate/invader_cards.py
@@ -1,4 +1,6 @@
-from .gamestate import GameState
+from enums import LandType
 
-class InvaderCard(GameState):
-    pass
+class InvaderCard():
+    def __init__(self, land_types: list[LandType], fear_phase: int):
+        self.fear_phase = fear_phase
+        self.land_types = land_types
